418d2c6cd77cb0c050ba85920d3db58031611af1
David Gauntlett <david.m.gauntlett@gmail.com>
2019-04-07 12:25:57 -0400

    Import assets, start with basic layout

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <v-app>
     <Header/>
     <v-content>
+      <TitleBar/>
     </v-content>
   </v-app>
 </template>
 
 <script>
 import Header from './components/Header/Header';
+import TitleBar from './components/TitleBar/TitleBar'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    TitleBar
   },
   data () {
     return {
---
+++ src/components/TitleBar/TitleBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-flex xs12>
+    <v-card dark color="primary">
+      <v-img :src="images.titleLogo"></v-img>
+      <v-card-text class="px-0">12</v-card-text>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+  export default {
+    name: "TitleBar",
+
+    data () {
+      return {
+        images: {
+          titleLogo: require('../../assets/Labs_Generic_Logo_White.png')
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
