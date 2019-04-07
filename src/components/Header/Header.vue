2490f4ebd1730112faed2d858b19bfe7df50b0aa
David Gauntlett <david.m.gauntlett@gmail.com>
2019-04-07 12:08:40 -0400

    remove old stock code, import fontawesome, start header

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-toolbar>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title>MAGLabs</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn icon>
+      <v-icon>fas fa-search</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+  export default {
+    name: "Header"
+  }
+</script>
+
+<style scoped>
+
+</style>
